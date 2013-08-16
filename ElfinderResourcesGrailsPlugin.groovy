694de571aab36dffe0656bba291053459faceb5e
Sudhir Nimavat <sudhir_nimavat@yahoo.com>
2013-08-16 17:36:32 +0530

    Initial import of plugin structure

---
+++ ElfinderResourcesGrailsPlugin.groovy
@@ -0,0 +1,12 @@
+class ElfinderResourcesGrailsPlugin {
+	def version = "0.1"
+	def grailsVersion = "1.3 > *"
+	def title = "Elfinder file explorer resources"
+	def author = "Sudhir Nimavat"
+	def authorEmail = "sudhir@nimavat.me"
+	def description = "Elfinder file explorer integration for Grails"
+	def documentation = "http://grails.org/plugin/elfinder-resources"
+	def scm = [system: 'GitHub', url: 'https://github.com/snimavat/grails-elfinder-connector']
+	def issueManagement = [ system: "GITHUB", url: "https://github.com/snimavat/grails-elfinder-connector/issues" ]
+	def license = "APACHE"   
+}
---
+++ grails-app/conf/Config.groovy
@@ -0,0 +1,9 @@
+log4j = {
+	appenders {
+		console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+	}
+	error 'org.codehaus.groovy.grails',
+			'org.springframework',
+			'org.hibernate',
+			'net.sf.ehcache.hibernate'
+}
